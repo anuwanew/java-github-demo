ae273fbeb1703ff21169c8f946a03a0ce4109de5
unknown <anuwanew@gmaile.com>
2025-05-15 11:27:47 +0530

    anu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hello from GitHub and Java!");
 System.out.println("anuwa");
+System.out.println("anu");
 }
 }
+
+
+
